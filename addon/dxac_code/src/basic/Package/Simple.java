d29d5f20beba6b4c2d9dbda241cf7e1284ab18df
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-03-17 15:18:54 +0530

    basic

---
+++ addon/dxac_code/src/basic/Package/Simple.java
@@ -0,0 +1,10 @@
+package basic.Package;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ addon/dxac_code/src/basic/concepts/Program.java
@@ -0,0 +1,12 @@
+package basic.concepts;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DXAC_CODE wolrld....");
+		
+		
+	}
+
+}
