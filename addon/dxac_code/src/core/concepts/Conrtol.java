376ad553e7046adf6e333d3d74f6ae18105ea701
Abdul Waheed A <abdulwaheeda638@gmail.com>
2025-03-19 15:10:38 +0530

    day 3

--- addon/dxac_code/src/core/concepts/Conrtol.java
+++ addon/dxac_code/src/core/concepts/Conrtol.java
@@ -7,87 +7,27 @@ import javax.print.DocFlavor.STRING;
 public class Conrtol {
 
 	public static void main(String[] args) {
-		// 1. Arithmetic Operators:
-				/*
-				int a = 10, b = 5;
-				
-				System.out.println("Addition: " + (a + b)); 
-				System.out.println("Subtraction: " + (a - b)); 
-				System.out.println("Multiplication: " + (a * b)); 
-				System.out.println("Division: " + (22.0 / 7)); 
-				System.out.println("Modulus: " + (10 % 3)); 
-				
-				// Operator precedence
-				System.out.println("Operator Precedence: " + 30 / (2 * 3));
-				
-				// Math Functions
-				System.out.println(Math.PI);
-				System.out.println(Math.sqrt(25));
-				System.out.println(Math.pow(5, 2));
-				*/
-				
-				// Relational Operators
-				/*
-				int a = 10, b = 5; 
-				System.out.println("a == b: " + (a == b)); 
-				System.out.println("a != b: " + (a != b)); 
-				System.out.println("a > b: " + (a > b)); 
-				System.out.println("a < b: " + (a < b)); 
-				System.out.println("a >= b: " + (a >= b)); 
-				System.out.println("a <= b: " + (a <= b));
-				*/
-				
-				// 3. Logical Operators
-				/*
-				boolean condition1 = true, condition2 = false;
-				
-				System.out.println("and && : " + (condition1 && condition2)); 
-				System.out.println("or || : " + (condition1 || condition2)); 
-				System.out.println("not ! : " + (!condition1));
-				*/
-				
-				
-				// 4. Unary Operators:
-				/*
-				int a = 10; 
-				boolean b = true; 
-				System.out.println("++a: " + (++a)); // Pre-increment 
-				System.out.println("a++: " + (a++)); // Post-increment 
-				System.out.println("Value of a after a++: " + a); 
-				System.out.println("--a: " + (--a)); // Pre-decrement 
-				System.out.println("a--: " + (a--)); // Post-decrement 
-				System.out.println("Value of a after a--: " + a); 
-				System.out.println("!b: " + (!b));
-				*/
-				
-				// 5. Assignment Operators:
-				/*
-				int a = 10;	
-				System.out.println(a);
-				System.out.println(++a);
-				// a = a + 2;
-				a += 2;
-				System.out.println(a);
-				a -= 3;
-				System.out.println(a);
-				*/
-				
-				// 6. Ternary Operators:
-				/*
-				int a = 10, b = 5;
-				
-				String result = (a < b) ? "a is greater" : "b is greater" ;
-				
-				System.out.println("Conditional Operator: " + result);
-				*/
-				
-				// 7. Bitwise Operators
-				
-				int a = 10, b = 4;
-				
-				System.out.println("Bitwise and (&): " + (a & b));
-				System.out.println("Bitwise or (|): " + (a | b));
-				System.out.println("Bitwise xor (^): " + (a ^ b));
-				System.out.println("Bitwise not (~): " + (~a));
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("ENTER THE MARITAL STATUS : ( married/ unmarried); " );
+	String ms = in.nextLine().toLowerCase();
+	
+	if(ms.equals("married")) {
+		System.out.println("YOU ARE ELIGIBLE TO THE INSURENCE...  ");
+		
+		}
+	else if (ms.equals("unmarried")) {
+		System.out.println("ENTER THE DETAILS...  ");
+		System.out.println("ENTER THE GENDER (MALE / FEMALE) :...  ");
+			String gender = in.nextLine().toLowerCase();
+			 
+	
+			if ( (gender.equals("male")) || (gender.equals("female"))) {
+				System.out.println("ENter you age :  ");
+				int age = in.nextInt();
+			}
+
+	}
+
 	}
 }
